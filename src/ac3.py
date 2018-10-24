5a8c03979b392b1867b8c96265bfc66b3700206a
MarkKokan <koka5160@mylaurier.ca>
2018-10-24 19:09:40 -0400

    changed way domain prints outs
    changed way domain prints outs

--- src/ac3.py
+++ src/ac3.py
@@ -1,25 +1,26 @@
 from sudoku import Sudoku
 
+
 def main():
-    InputFile = open('sudoku.txt','r')
+    InputFile = open('sudoku.txt', 'r')
 
     game_board = list()
 
     for line in InputFile:
         game_board += [int(i) for i in line.split(',')]
     InputFile.close
-    #Close the text file
+    # Close the text file
 
-    #Create the sudoku object
+    # Create the sudoku object
     sudoku = Sudoku(game_board)
 
-
-    #Print properties for debugging
+    # Print properties for debugging
     print(sudoku.variables)
-    print(sudoku.domain)
 
+    for x in sudoku.domain:
+        print(x + ": ", end='')
+        print(sudoku.domain[x])
 
 
 if __name__ == '__main__':
     main()
-    
