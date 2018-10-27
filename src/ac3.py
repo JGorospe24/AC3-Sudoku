be9a659512b9737a27e8503ca5fab2e1a22dd0c0
JGorospe24 <jaenuzagorospe@gmail.com>
2018-10-27 00:24:28 -0400

    tesT

--- src/ac3.py
+++ src/ac3.py
@@ -1,7 +1,7 @@
 from sudoku import Sudoku
 
 def main():
-    InputFile = open('sudoku.txt', 'r')
+    InputFile = open("sudoku.txt", "r")
 
     game_board = list()
 
@@ -14,7 +14,18 @@ def main():
     board = Sudoku(game_board)
 
     # Print properties for debugging
+<<<<<<< Updated upstream
     print(board.variables)
+=======
+    print(sudoku.variables)
+
+    for x in sudoku.domain:
+        print(x + ": ", end='')
+        print(sudoku.domain[x])
+
+    sudoku.create_constraints() #Just for testing right now
+
+>>>>>>> Stashed changes
 
     #or x in board.domain:
     #   print(x + ": ", end='')
