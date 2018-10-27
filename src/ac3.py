65f9d2c3b1e1839d83935837e86c04ab9bdbb4dc
unknown <harjodh.saini@gmail.com>
2018-10-27 10:55:53 -0400

    formatting fixes

--- src/ac3.py
+++ src/ac3.py
@@ -1,24 +1,26 @@
 from sudoku import Sudoku
 
+
 def main():
-    InputFile = open('sudoku.txt', 'r')
+    InputFile = open('sudoku.txt')
 
     game_board = list()
 
     for line in InputFile:
         game_board += [int(i) for i in line.split(',')]
-    InputFile.close
     # Close the text file
+    InputFile.close
 
     # Create the sudoku object
     board = Sudoku(game_board)
 
     # Print properties for debugging
-    #print(board.variables)
-    #or x in board.domain:
+    # print(board.variables)
+    # or x in board.domain:
     #   print(x + ": ", end='')
     #    print(board.domain[x])
-    #board.create_constraints() #Just for testing right now
+    # board.create_constraints() #Just for testing right now
+
 
 if __name__ == '__main__':
     main()
