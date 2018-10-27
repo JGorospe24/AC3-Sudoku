735ac481cf181a0e5e0b14ba8cd9f462a1f6f1e7
unknown <harjodh.saini@gmail.com>
2018-10-27 11:22:37 -0400

    added ac3 and revise skelton

--- src/ac3.py
+++ src/ac3.py
@@ -24,3 +24,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# returns false if domain size = 0 else returns true
+def AC3(board):
+    arc_queue = list() #queue of arcs (intially all constraints)
+    while arc_queue:
+        current_arc = arc_queue.pop()
+        if revise(board,current_arc[0],current_arc[1]):
+
+# takes board, current_arc[0](x1) and current_arc[1](x2) as parameters
+# returns true iff domain x1 is revised
+def revise(board,x1,x2):
