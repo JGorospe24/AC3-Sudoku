f850fd2a0b69ec83a4a8a8e282a25566cadbb049
JGorospe24 <jaenuzagorospe@gmail.com>
2018-10-31 14:19:28 -0400

    Began backtracking algorithm and implemented the 2 heuristics: minimum remaining values and least_constraining value

--- src/ac3.py
+++ src/ac3.py
@@ -20,7 +20,6 @@ def AC3(board):
 
 # takes board, current_arc[0](x1) and current_arc[1](x2) as parameters
 # returns true iff domain x1 is revised
-
 def revise(board, x1, x2):
     revised = False
     # check each value in the domain of x1
@@ -30,7 +29,49 @@ def revise(board, x1, x2):
             revised = True
     return revised
 
-def backtrack(csp):
+def backtrack(assignments, csp):
+    if len(assignments) == len(csp.variables):
+        return assignments
+    currentVar = minimum_remaining_values(assignments, csp)
+    for v in least_constraining_value(currentVar, csp):
+        if isConsistent(assignments,currentVar, v, csp):
+
+def define_assigned_vars(csp):
+    assigned = dict()
+    for x in csp.variables:
+        if len(csp.domain[x]) == 1:
+            assigned[x] = csp.domain[x][0]
+    return assigned
+
+def minimum_remaining_values(assignments, csp):
+    unassigned = list()
+    for x in csp.variables:
+        if x not in assignments:
+            unassigned.append(x)
+    min_var = min(unassigned, key=lambda var: len(csp.domain[var]))
+    return min_var
+
+def least_constraining_value(var, csp):
+    if len(csp.domain[var]) == 1:
+        return csp.domain[var]
+    sorted_domain = sorted(csp.domain[var], key=lambda v: constraints(var, v, csp))
+    return sorted_domain
+
+def constraints(var, val, csp):
+    constraints = 0
+    for x in csp.neighbours[var]:
+        if len(csp.domain[x]) > 1:
+            if x in csp.domain[x]:
+                constraints += 1
+    return constraints
+
+def isConsistent(assignments, var, val, csp):
+    consistent = True
+    for key in assignments:
+        if assignments[key] == val:
+            if key in csp.neighbours[var]:
+                consistent = False
+    return consistent
 
 
 def main():
@@ -46,9 +87,7 @@ def main():
     # Create the sudoku object
     board = Sudoku(game_board)
 
-    if not AC3(board):
-        print("No Solution Found")
-    else:
+    if AC3(board):
         print("Solution Found")
         # print(board.domain)
         print("|", end="")
@@ -66,6 +105,10 @@ def main():
                 print("  |", end="")
             print("{}|".format(board.domain[x][0]), end="")
             count += 1
+    else:
+        assigned = define_assigned_vars(board)
+
+
 
 if __name__ == '__main__':
     main()
